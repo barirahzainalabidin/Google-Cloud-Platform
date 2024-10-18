b6ec484b61cd13afd4855a3da9fafb56b156f791
Barirah Zainal Abidin <113294657+barirahzainalabidin@users.noreply.github.com>
2024-10-18 12:49:35 +0800

    Update evaluation-model (2).sql

--- Bigquery/Linear-Regression/evaluation-model (2).sql
+++ Bigquery/Linear-Regression/evaluation-model (2).sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE `machine-learning-1-438523.housenew.evaluation_model` AS
+CREATE OR REPLACE TABLE `train-1.housenew.evaluation_model` AS
 SELECT
     YrSold AS year_sold,
     SalePrice AS total_sale_price,
@@ -14,7 +14,7 @@ SELECT
     GarageCars AS garage_cars,
     Fireplaces AS fireplaces
 FROM
-    `machine-learning-1-438523.housenew.imputed_price`
+    `train-1.housenew.imputed_price`
 WHERE
     (YrSold = 2006 AND (OverallQual >= 5 OR OverallCond >= 5))
     AND SalePrice >= 100000
