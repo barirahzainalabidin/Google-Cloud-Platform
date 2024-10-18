cd78117e349686b43156e8366835c3d9e0d3de9c
Barirah Zainal Abidin <113294657+barirahzainalabidin@users.noreply.github.com>
2024-10-18 12:44:52 +0800

    Update simple-imputer.sql

--- Bigquery/Arima/simple-imputer.sql
+++ Bigquery/Arima/simple-imputer.sql
@@ -1,6 +1,6 @@
-CREATE OR REPLACE TABLE `machine-learning-1-438523.housenew.imputed_price` AS
+CREATE OR REPLACE TABLE `train-1.housenew.imputed_price` AS
 SELECT
   *,
   IFNULL(SalePrice, AVG(SalePrice) OVER ()) AS imputed_sale_price
 FROM 
-  `machine-learning-1-438523.housenew.price`;
+  `train-1.housenew.price`;
