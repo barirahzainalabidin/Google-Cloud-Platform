bcc650fb85238ee30c1314974f7422c64b50848f
Barirah Zainal Abidin <113294657+barirahzainalabidin@users.noreply.github.com>
2024-10-18 19:11:27 +0800

    Update prepare_data.sql

--- Bigquery/Loan Approval Prediction/prepare_data.sql
+++ Bigquery/Loan Approval Prediction/prepare_data.sql
@@ -1,9 +1,9 @@
-CREATE OR REPLACE TABLE machine-learning-1-438523.new_loan.prepare_data AS
+CREATE OR REPLACE TABLE train-1.new_loan.prepare_data AS
 SELECT *
-FROM machine-learning-1-438523.new_loan.loan
+FROM train-1.new_loan.loan
 WHERE MOD(ABS(FARM_FINGERPRINT(CAST(id AS STRING))), 10) < 8;
 
 CREATE OR REPLACE TABLE machine-learning-1-438523.new_loan.loan AS
 SELECT *
-FROM machine-learning-1-438523.new_loan.loan
+FROM train-1.new_loan.loan
 WHERE MOD(ABS(FARM_FINGERPRINT(CAST(id AS STRING))), 10) >= 8;
